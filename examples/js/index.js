e58508c30eeca8384679e44041b4d750de09873f
nuintun <nuintun@qq.com>
2016-12-13 17:39:13 +0800

    update files

--- examples/js/index.js
+++ examples/js/index.js
@@ -9,21 +9,19 @@ function create() {
     buttons: [{
       which: 13,
       label: '确认',
-      className: 'ui-button ui-button-yes',
+      className: 'ui-button',
       action: function() {
         this.close();
       }
     }, {
       which: 27,
       label: '取消',
-      className: 'ui-button ui-button-no',
+      className: 'ui-button ui-button-white',
       action: function() {
         this.close();
       }
     }]
   });
-
-  // dialog.innerHTML = '<div class="ui-dialog-content"><span>hello, world</span><a class="button ui-close">关闭</a></div>';
 
   var reset = function() {
     dialog.reset();
@@ -31,45 +29,45 @@ function create() {
 
   __window.on('resize', reset);
 
-  dialog.on('focus', function() {
-    console.log('focus');
-  });
+  // dialog.on('focus', function() {
+  //   console.log('focus');
+  // });
 
-  dialog.on('blur', function() {
-    console.log('blur');
-  });
+  // dialog.on('blur', function() {
+  //   console.log('blur');
+  // });
 
-  dialog.on('show', function() {
-    console.log('show');
-  });
+  // dialog.on('show', function() {
+  //   console.log('show');
+  // });
 
-  dialog.on('beforeclose', function() {
-    console.log('beforeclose');
-  });
+  // dialog.on('beforeclose', function() {
+  //   console.log('beforeclose');
+  // });
 
-  dialog.on('close', function() {
-    console.log('close');
+  // dialog.on('close', function() {
+  //   console.log('close');
 
-    if (remove.checked) {
-      dialog.remove();
+  //   if (remove.checked) {
+  //     dialog.remove();
 
-      popup = null;
-    }
-  });
+  //     popup = null;
+  //   }
+  // });
 
-  dialog.on('beforeremove', function() {
-    console.log('beforeremove');
-  });
+  // dialog.on('beforeremove', function() {
+  //   console.log('beforeremove');
+  // });
 
   dialog.on('remove', function() {
-    console.log('remove');
+    // console.log('remove');
 
     __window.off('resize', reset);
   });
 
-  $(dialog.node).on('click', '.ui-close', function() {
-    dialog.close();
-  });
+  // $(dialog.node).on('click', '.ui-close', function() {
+  //   dialog.close();
+  // });
 
   return dialog;
 }
